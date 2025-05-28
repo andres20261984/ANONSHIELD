2b82d076cef3e32c45b6857e840380348cddc03d
andres20261984 <sentella198403@gmail.com>
2025-05-28 15:17:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
 app.use(express.static("public"));
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+app.get("/generate", (req, res) => {
+  const domain = "anonshield.email";
+  const name = Math.random().toString(36).substring(2, 10);
+  const email = `${name}@${domain}`;
+  res.json({ email });
+});
+
+app.listen(PORT, () => console.log("Servidor corriendo en puerto " + PORT));
